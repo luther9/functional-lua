47bdb406da0d0f6cae7c83da20891da4a4c620d1
Luther Thompson <lutheroto@gmail.com>
2019-04-12 14:20:53 -0400

    Create array module.

---
+++ test-all.lua
@@ -0,0 +1,24 @@
+#!/usr/bin/env lua
+
+--[[
+Copyright 2018 Luther Thompson
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License (GPL3) as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+You have the following additional permission: You may convey the program in
+object code form under the terms of sections 4 and 5 of GPL3 without being
+bound by section 6 of GPL3.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]
+
+require'test-array'
